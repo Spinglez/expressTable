1220de19fe09a85ca6cccf5855b7cc99f6d997dc
akp28 <ajaypaladugu@gmail.com>
2018-12-12 19:47:49 -0800

    basic routes

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ const keys =  dotenv.config();
 const port = keys.parsed.PORTNUM
 
 console.log(port);
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
